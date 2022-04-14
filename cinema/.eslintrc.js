1992f262a6d819a3e5d5607178a00683dbcdabb2
Melya <melenko55@gmail.com>
2022-04-15 01:24:45 +0300

    Redux + Header_part

--- cinema/.eslintrc.js
+++ cinema/.eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
         "browser": true,
         "es2021": true
     },
-    "extends": "plugin:react/recommended",
+    "extends": ["plugin:react/recommended", "plugin:react/jsx-runtime"],
     "parserOptions": {
         "ecmaFeatures": {
             "jsx": true
@@ -16,11 +16,14 @@ module.exports = {
     ],
     "rules": {
         "no-invalid-this": "error",
-        "arrow-parens": ["error", "as-needed"],
+        // "arrow-parens": ["error", "as-needed"],
         "no-duplicate-imports": "error",
         "no-useless-constructor": "error",
         "no-useless-rename": "error",
         "no-var": "error",
-        "max-len": ["error", { "code": 80 }]
+        "max-len": ["error", { "code": 100 }],
+        "react/jsx-uses-react": "off",
+        "react/react-in-jsx-scope": "off",
+        "react/prop-types": 0
     }
 }
---
+++ cinema/src/configureStore.js
@@ -0,0 +1,13 @@
+import { configureStore } from '@reduxjs/toolkit'
+import filmReducer from './reducers/filmSlice'
+import scheduleReducer from './reducers/scheduleSlice'
+import commentReducer from './reducers/commentSlice'
+
+export default configureStore({
+    reducer: {
+        film: filmReducer,
+        schedule: scheduleReducer,
+        comment: commentReducer
+    },
+    // middleware: []
+})
